60b8715584ee15ac625687e41d18a4a49ddd3cfa
Miftah Afina <miftah.afina@gmail.com>
2021-11-08 11:15:41 +0700

    Arithmetic operator

---
+++ bin/11.arithmetic.dart
@@ -0,0 +1,20 @@
+void main() {
+  /**
+   * +
+   * -
+   * *
+   * /   = pembagian hasil double
+   * ~/ = pembagian hasil int
+   * %  = sisa bagi
+   */
+
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+}
