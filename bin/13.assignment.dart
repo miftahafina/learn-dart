bc6ad5080cb2fd08a7098742cae31ebc8cad2774
Miftah Afina <miftah.afina@gmail.com>
2021-11-08 11:31:16 +0700

    Increment and decrement

--- bin/13.assignment.dart
+++ bin/13.assignment.dart
@@ -2,10 +2,12 @@ void main() {
   var a = 100;
   var b = 100.0;
 
-  print(a += 10);
-  print(a -= 10);
-  print(a *= 10);
-  print(b /= 10);
-  print(a ~/= 10);
-  print(a %= 10);
+  a += 10;
+  // a -= 10;
+  // a *= 10;
+  // b /= 10;
+  // a ~/= 10;
+  // a %= 10;
+
+  print(a);
 }
