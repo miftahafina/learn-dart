930b4990e57f12a2c988ed3c44d650651317d884
Miftah Afina <miftah.afina@gmail.com>
2021-11-08 11:19:30 +0700

    Comparison operator

---
+++ bin/12.comparison.dart
@@ -0,0 +1,14 @@
+void main() {
+  int first = 4;
+  double second = 4.5;
+  String third = '4.5';
+
+  print(first == second);
+  print(first != second);
+  print(first > second);
+  print(first < second);
+  print(first >= second);
+  print(first <= second);
+
+  print(second == third);
+}
