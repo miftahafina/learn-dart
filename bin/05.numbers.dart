0a6d958a51e82c86c5ce1f230454634532f8b2b9
Miftah Afina <miftah.afina@gmail.com>
2021-11-08 10:35:06 +0700

    Numbers

---
+++ bin/05.numbers.dart
@@ -0,0 +1,12 @@
+void main() {
+  int    number1 = 10;
+  double number2 = 10.23;
+  num    number3 = 8;      // int or double
+
+  print(number1);
+  print(number2);
+  print(number3);
+
+  number3 = 8.2;
+  print(number3);
+}
