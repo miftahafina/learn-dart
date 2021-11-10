ddd4b5099138ec7e70f0b751a055581b30605a43
Miftah Afina <miftah.afina@gmail.com>
2021-11-10 09:36:03 +0700

    If else

---
+++ bin/21.if_else.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool belajar  = true;
+  bool berdoa   = false;
+  bool anakKepsek = true;
+
+  if (belajar && berdoa) {
+    print('Naik kelas');
+  } else if (anakKepsek) {
+    print('Bisa diatur');
+  } else {
+    print('Tidak naik kelas');
+  }
+}
