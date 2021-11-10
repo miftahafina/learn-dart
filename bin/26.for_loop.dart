ce5a534f933edd8b67d4791aef0969fcab4657b8
Miftah Afina <miftah.afina@gmail.com>
2021-11-10 10:15:51 +0700

    For loop

---
+++ bin/26.for_loop.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<String> marioSeries = [
+    'Super Mario Bros.',
+    'Super Mario 3D Land',
+    'Super Mario Galaxy',
+  ];
+
+  // For in
+  for (var mario in marioSeries) {
+    print(mario);
+  }
+
+  // For
+  for (var i = 0; i <= 10; i++) {
+    print(i);
+  }
+}
