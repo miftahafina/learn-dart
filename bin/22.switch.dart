c6af8062f23278d0c078217741522eae8d206e90
Miftah Afina <miftah.afina@gmail.com>
2021-11-10 09:45:39 +0700

    Switch

---
+++ bin/22.switch.dart
@@ -0,0 +1,15 @@
+void main() {
+  String nilai = 'B';
+
+  switch (nilai) {
+    case 'A':
+    case 'B':
+      print('Lulus');
+      break;
+    case 'C':
+      print('Remidi');
+      break;
+    default:
+      print('Tidak lulus');
+  }
+}
