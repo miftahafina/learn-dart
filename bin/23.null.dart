2359bb0acb4f5729a3cd4f02bdce4fe93782b420
Miftah Afina <miftah.afina@gmail.com>
2021-11-10 09:51:51 +0700

    Null

---
+++ bin/23.null.dart
@@ -0,0 +1,10 @@
+void main() {
+  // secara default, semua var harus isi data,
+  // jika tidak maka tidak bisa digunakan
+  // agar bisa diisi dengan null, maka dikasih tanda tanya
+  // nullable
+  
+  // String? prestasi = null;
+  String? prestasi;
+  print(prestasi);
+}
